7f225d7c6edfd7c8150e7c75999b5ec5595fb948
danielramke <genshin.exepta@outlook.com>
2022-11-03 11:21:56 +0100

    fixing the password regex. Was tested by the test classes successfully.

--- src/main/java/net/vogeez/authorization/service/validate/PasswordValidator.java
+++ src/main/java/net/vogeez/authorization/service/validate/PasswordValidator.java
@@ -12,7 +12,19 @@ import java.util.regex.Pattern;
  */
 public class PasswordValidator implements ConstraintValidator<Password, String> {
 
-    public final static Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@+-§$%&/(){}=?!<>|]){8,32}$");
+    /*
+     * Password Credentials:
+     *
+     * Min length: 8 chars
+     * Max length: 32 chars
+     * One lowercase letter
+     * One uppercase letter
+     * One special character
+     * One number
+     *
+     * Note: the regex check the input string for blank chars! (Whitespace)
+     */
+    public final static Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^§&+=])(?=\\S+$).{8,32}$");
 
     @Override
     public void initialize(Password constraintAnnotation) {
